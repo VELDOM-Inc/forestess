231e03c99759573c543f7824e56b822633d71f84
Kanae.Orihara <orihara@veldom.co.jp>
2025-03-17 15:18:21 +0900

    事業内容ページ作成

---
+++ public_html/forestess/common/js/service.js
@@ -0,0 +1,58 @@
+/* ============================================================ */
+/* slick */
+/* ============================================================ */
+$(function(){
+	$('._slider01_').slick({
+		arrows: false,
+		centerMode: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		speed: 1000,
+		cssEase: 'linear',
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		pauseOnDotsHover: false,
+		variableWidth: false,
+		lazyLoad: 'progressive',
+		responsive: [
+			{
+			breakpoint: 950,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+			}
+		}
+		]
+	});
+	
+	$('._slider02_').slick({
+			arrows: false,
+			dots: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: false,
+			autoplaySpeed: 3000,
+			speed: 1000,
+			cssEase: 'linear',
+			pauseOnFocus: false,
+			pauseOnHover: false,
+			pauseOnDotsHover: false,
+			variableWidth: false,
+			lazyLoad: 'progressive',
+			responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+				}
+			}
+			]
+	});
+});
+
