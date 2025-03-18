9d1ab5c529c0adf7dd6a4f0469b23c0ca37e1998
Kanae.Orihara <orihara@veldom.co.jp>
2025-03-18 15:00:45 +0900

    ページスクロールアニメーション追加

--- public_html/forestess/common/js/index.js
+++ public_html/forestess/common/js/index.js
@@ -1,60 +1,15 @@
-/* ============================================================ */
-/* slick */
-/* ============================================================ */
-$(function(){
-	$('._slider01_').slick({
-		infinite: true, 
-		arrows: false, 
-		fade: true, 
-		speed: 2000, 
-		autoplaySpeed: 5000, 
-		autoplay: true, 
-		pauseOnFocus: false, 
-		pauseOnHover: false,
-		pauseOnDotsHover: false,
-		accessibility: false,
-		responsive: [
-		{
-			breakpoint: 767,
-			settings: {
-				slidesToShow: 2,
-			}
-		}
-		]
-	});
-});
+// CSSアニメーションを間隔を空けてループ再生させる処理
+function looopAnimation(id, className, delay) {
+  var element = document.getElementById(id);
+  element.addEventListener("animationend", listener);
+  
+  function listener(event) {
+    event.target.classList.remove(className);
+    setTimeout(playAnimation, delay);
+  }
 
-/* 事業内容 */
-$('.c-index-company-detail__list > li > a').hover(function () {
-	if (window.matchMedia('(min-width:768px)').matches && !window.matchMedia('(hover: none)').matches) {
-		var index = $('.c-index-company-detail__list > li > a').index($(this));
-		$('.c-index-company-img__item.isShow').removeClass('isShow');
-		$('.c-index-company-img__item').eq(index).addClass('isShow');
-		
-		$('.c-index-company-detail__link.isShow').removeClass('isShow');
-		$('.c-index-company-detail__link').eq(index).addClass('isShow');
-	}
-});
-
-if ($('.c-index-company-img__item').length > 0) {
-	$('.c-index-company-img__item').eq(0).addClass('isShow');
-	if (window.matchMedia('(max-width:767px)').matches || window.matchMedia('(hover: none)').matches) {
-		$('.c-index-company-detail__list > li > a').eq(0).addClass('isShow');
-		
-		$('.c-index-company-detail__link').eq(0).addClass('isShow');
-	}
+  function playAnimation() {
+    element.classList.add(className);
+  }
 }
 
-if (window.matchMedia('(max-width:767px)').matches && $('.c-index-company-img__item').length > 0) {
-	setInterval(function () {
-		var index = $('.c-index-company-img__item').index($('.c-index-company-img__item.isShow')),
-			next = index + 1;
-		if (next === $('.c-index-company-img__item').length) {
-			next = 0;
-		}
-		$('.c-index-company-detail__list > li > a.isShow').removeClass('isShow');
-		$('.c-index-company-detail__list > li > a').eq(next).addClass('isShow');
-		$('.c-index-company-img__item.isShow').removeClass('isShow');
-		$('.c-index-company-img__item').eq(next).addClass('isShow');
-	}, 5000);
-}
